2eef539e8a23d023fc051de33699acd30f33c3d2
frits-scholer <frits.scholer@gmail.com>
2015-09-17 12:27:36 +0200

    Changed testfind to find two adjacent characters

--- README.md
+++
@@ -1,2 +0,0 @@
-# comb
-combinatorial
--- testfind.cpp
+++ testfind.cpp
@@ -6,14 +6,11 @@ using namespace std;
 
 string buf;
 
-bool two_in_a_row (char c) {
-  return (c == 'd');
-}
 int main() {
   getline(cin, buf);
   cout << buf << '\n';
-  string::iterator bufi = find_if(buf.begin(),buf.end(),two_in_a_row);
-  if (bufi != buf.end()) cout << *bufi << *bufi << " at " << int(bufi-buf.begin()) << '\n';
+  string::iterator bufi = adjacent_find(buf.begin(),buf.end());
+  if (bufi != buf.end()) cout << "two " << *bufi << " chars at position " << int(bufi-buf.begin()) << '\n';
   else cout << "no two characters in a row" << '\n';
 
 }
