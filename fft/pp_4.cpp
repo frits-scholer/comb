b243413164c4931e6c151d13afef673d6adfcc1d
frits-scholer <frits.scholer@gmail.com>
2019-06-23 18:46:41 +0200

    FFT learning

---
+++ fft/pp_4.cpp
@@ -0,0 +1,178 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+#include <iostream>
+#include <set>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <complex>
+#include <map>
+#include <queue>
+#include <array>
+#include <bitset>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<vi> vvi;
+typedef long double ld;
+typedef vector<double> vd;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef vector<pii> vii;
+typedef vector<string> vs;
+//typedef long long int
+const int mod = 7340033;
+
+ll mpow(ll x, ll n, int mod) {
+  ll r = 1;
+  while (n) {
+    if (n & 1) r = r * x % mod;
+    x = x * x % mod;
+    n /= 2;
+  }
+  return r;
+}
+
+int dw(int x, int mod) {
+  if (x < mod) return x;
+  return x - mod;
+}
+
+int g;
+int root = 1;
+int root_1 = mpow(root, mod-2, mod);
+int root_pw = 0;
+const int max_root_pw = 1 << 17;
+int W[2][max_root_pw];
+void precalcw() {
+  for (g = 2; g < mod - 1; ++g) {
+    if (mpow(g, (mod - 1) / 2, mod) != 1) {
+      break;
+    }
+  }
+  root = mpow(g, (mod - 1) / root_pw, mod);
+  root_1 = mpow(root, mod-2, mod);
+  unsigned long long r[2] = {root, root_1};
+  for (int t = 0; t < 2; ++t) {
+    W[t][0] = 1;
+    for (int i = 1; i < root_pw; ++i) {
+      W[t][i] = W[t][i-1] * r[t] % mod;
+    }
+  }
+}
+void fft (vector<int> & a, int invert) {
+  int n = (int) a.size();
+  if (n != root_pw) {
+    root_pw = n;
+    precalcw();
+  }
+  for (int i = 0; i < n; ++i) if (a[i] < 0) a[i] += mod;
+
+  for (int i=1, j=0; i<n; ++i) {
+    int bit = n >> 1;
+    for (; j>=bit; bit>>=1)
+      j -= bit;
+    j += bit;
+    if (i < j)
+      swap (a[i], a[j]);
+  }
+
+  for (int len=2, t = n/len; len<=n; len<<=1, t/=2) {
+    for (int i=0; i<n; i+=len) {
+      for (int j=0, pw = 0; j<len/2; ++j, pw += t) {
+        int u = a[i+j];
+        int v = a[i+j+len/2] * (unsigned long long)W[invert][pw] % mod;
+        a[i+j] = dw(u+v, mod);
+        a[i+j+len/2] = u-v >= 0 ? u-v : u-v+mod;
+      }
+    }
+  }
+  if (invert) {
+    unsigned long long nrev = mpow(n, mod-2, mod);
+    for (int i=0; i<n; ++i)
+      a[i] = a[i] * nrev % mod;
+  }
+}
+
+vi smul(vi a, vi b) {
+  int pw = a.size() + b.size();
+  vi c(pw);
+  for (int i = 0; i < a.size(); ++i) for (int j = 0; j < b.size(); ++j) {
+    c[i+j] = (c[i+j] + a[i] *(ll)b[j]) % mod;
+  }
+  return c;
+}
+
+vi fmul(vi a, vi b) {
+  int pw = a.size() + b.size();
+  assert(pw < max_root_pw);
+  while (a.size() < max_root_pw) a.push_back(0);
+  while (b.size() < max_root_pw) b.push_back(0);
+  fft(a, 0);
+  fft(b, 0);
+  for (int i = 0; i < a.size(); ++i) a[i] = a[i] * (ll)b[i] % mod;
+  fft(a, 1);
+  for (int i = pw; i < a.size(); ++i) assert(a[i] == 0);
+  a.resize(pw);
+  return a;
+}
+
+vi fac(mod, 1);
+
+ll fc(int n) {
+  if (n < fac.size()) return fac[n];
+  return 0;
+//  return fac[n % mod] * fc(n / mod) % mod;
+}
+
+void out(vi v) {
+  for (int x : v) cerr << x << ' ';
+  cerr << endl;
+}
+
+int solve(int n, int a, int b, int c) {
+  ll mul = mpow(2, mod - 1 - a - b, mod) % mod;
+//  assert(mod > n);
+/*  if (mod <= n) {
+    cout << 0 << endl;
+    return 0;
+  }*/
+  ll res = 0;
+  vi x(a+1), y(b+1);
+  for (int i = 0; i <= a; ++i) {
+    x[i] = fac[a] * mpow(fac[i] * (ll)fac[a-i] % mod, mod-2, mod) % mod;
+    if ((a-i) % 2) x[i] = dw(mod - x[i], mod);
+  }
+  for (int i = 0; i <= b; ++i) {
+    y[i] = fac[b] * mpow(fac[i] * (ll)fac[b-i] % mod, mod-2, mod) % mod;
+  }
+  vi z = fmul(x, y);
+//  out(x); out(y); out(z);
+  for (int i = 0; i < z.size(); ++i) {
+    int pw = c - a - b + 2 * i;
+    res = (res + z[i] * mpow(pw, n, mod)) % mod;
+  }
+  return (res % mod * mul % mod + mod) % mod;
+}
+
+int stupid(int n, int a, int b, int c) {
+  ll res = 0;
+  return (res % mod + mod) % mod;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  for (int i = 2; i < fac.size(); ++i) {
+    fac[i] = fac[i-1] * (ll)i % mod;  
+  }
+  int n,a,b,c;
+  cin >> n >> a >> b >> c;
+  cout << solve(n, a, b, c) << endl;
+  return 0;
+}
---
+++ fft/test_pp.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int mod = 7340033;
+const int r = 2187;
+const int K = 21;
+const int M = 20;
+const int N = 1 << M;
+
+#define long long long
+
+long rPower[2][K];
+int reversedBits[N];
+long A[N], B[N];
+
+long bin(long x, long a) {
+    x %= mod;
+    long y = 1;
+    while (a) {
+        if (a & 1)
+            y = (y * x) % mod;
+        x = (x * x) % mod;
+        a >>= 1;
+    }
+    return y;
+}
+
+long inv(long x) {
+    return bin(x, mod - 2);
+}
+
+void pre() {
+    rPower[0][K - 1] = r;
+    rPower[1][K - 1] = inv(r);
+    for (int j = 0; j < 2; ++j)
+    for (int i = K - 2; i >= 0; --i)
+        rPower[j][i] = bin(rPower[j][i + 1], 2);
+
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < M; ++j)
+            if ((i >> j) & 1)
+                reversedBits[i] |= (1 << (M - j));
+}
+
+int reverseBits(int x, int ns) {
+    return reversedBits[x] >> (M - ns + 1);
+}
+
+void fft(long *a, int n, int ns, long *power) {
+    for (int i = 0; i < n; ++i)
+        if (reverseBits(i, ns) > i)
+            swap(a[i], a[reverseBits(i, ns)]);
+
+    for (int j = 0; j < ns; ++j)
+        for (int i = 0; i < n; i += (1 << (j + 1))) {
+            long p = 1, e = power[j + 1];
+            for (int k = 0; k < (1 << j); ++k) {
+                long x = a[i + k], y = a[i + k + (1 << j)];
+                a[i + k] = (x + p * y) % mod;
+                a[i + k + (1 << j)] = (x - p * y) % mod;
+                p = (p * e) % mod;
+            }
+        }
+}
+
+void mul(long *a, long *b, long *c, int ns) {
+    fft(a, 1 << ns, ns, rPower[0]);
+    fft(b, 1 << ns, ns, rPower[0]);
+    for (int i = 0; i < (1 << ns); ++i)
+        c[i] = (a[i] * b[i]) % mod;
+    fft(c, 1 << ns, ns, rPower[1]);
+    long in = inv(1 << ns);
+    for (int i = 0; i < (1 << ns); ++i) {
+        c[i] = (c[i] * in) % mod;
+        if (c[i] < 0)
+            c[i] += mod;
+    }
+}
+
+int n, a, b, c;
+long f[N], fr[N];
+
+long binom(long n, long k) {
+    if (k < 0 || n < k)
+        return 0;
+    return (f[n] * fr[k] % mod) * fr[n - k] % mod;
+}
+
+void kill() {
+    cin >> n >> a >> b >> c;
+
+    f[0] = fr[0] = 1;
+    for (int i = 1; i < N; ++i) {
+        f[i] = (i * f[i - 1]) % mod;
+        fr[i] = inv(f[i]);
+    }
+
+    for (int i = 0; i <= a; ++i)
+      A[i] = 1;//(i % 2 == 0 ? 1 : -1) * binom(a, i);
+
+    for (int i = 0; i <= b; ++i)
+      B[i] = 1;//binom(b, i);
+
+    int ns = 0;
+    while ((1 << ns) <= a + b)
+        ++ns;
+
+    for (int i = 0; i <= a; ++i)
+        cerr << A[i] << " ";
+    cerr << endl;
+
+    for (int i = 0; i <= b; ++i)
+        cerr << B[i] << " ";
+    cerr << endl;
+
+    mul(A, B, A, ns);
+
+
+    for (int i = 0; i <= a + b; ++i)
+        cerr << A[i] << " ";
+    cerr << endl;
+
+    // long ans = 0;
+    // for (int i = 0; i <= a + b; ++i) {
+    //     long x = a + b + c - 2 * i;
+    //     ans = (ans + A[i] * bin(x, n)) % mod;
+    // }
+
+    // ans *= bin(inv(2), a + b);
+    // ans %= mod;
+
+    // if (ans < 0)
+    //     ans += mod;
+    // cout << ans << endl;
+}
+
+int main() {
+    pre();
+    kill();
+    return 0;
+}
